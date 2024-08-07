f21048be525cc914a8eef8586898f82fa4c422cc
Faisal Ahmed <git@fais.al>
2024-08-07 12:52:29 +0200

    asdf

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const port = 5010;
+const port = process.env.port || 5010;
 
 const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${server.address().port}`);
