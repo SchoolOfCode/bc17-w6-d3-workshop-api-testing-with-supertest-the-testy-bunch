aa597d3d30d3e828cf55b1ea49f6603648ee638b
Maria <maria.g1611@yahoo.com>
2024-08-07 16:13:44 +0100

    part of ticket 6 attempted

--- users/users.test.js
+++ users/users.test.js
@@ -1,8 +1,7 @@
 import request from "supertest";
 import { test, expect } from "vitest";
 import app from "../app.js";
-import { json } from "express";
-
+import {resetUsersTable} from "../db/helpers.js";
 
 test("Health check route should work!", async () => {
 
@@ -14,3 +13,17 @@ test("Health check route should work!", async () => {
 
 })
 
+
+test("Users route should be functional", async () => {
+	
+	let result = await request(app).get("/api/users");
+	expect(result.statusCode).toBe(200);
+    expect(result.body.success).toBe(true)
+	expect(Array.isArray(result.body.payload)).toBe(true)
+	expect('id' in result.body.payload[0]).toBe(true)
+	expect('username' in result.body.payload[0]).toBe(true)
+	expect(result.headers["content-type"]).toBe("application/json; charset=utf-8")
+})
+ 
+ 
+//we first checked if payload is actually an Array  expect(Array.isArray(result.body.payload)).toBe(true)
