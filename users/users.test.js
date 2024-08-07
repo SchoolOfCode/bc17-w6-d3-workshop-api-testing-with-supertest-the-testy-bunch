6e8a478387dc96c109b1bcbc130bf8d9287d55e2
Eric-0805 <ericroberts805@gmail.com>
2024-08-07 16:32:24 +0100

    ticket 5 and 6

--- users/users.test.js
+++ users/users.test.js
@@ -1,29 +1,39 @@
 import request from "supertest";
-import { test, expect } from "vitest";
+import { test, expect, beforeEach } from "vitest";
 import app from "../app.js";
-import {resetUsersTable} from "../db/helpers.js";
+import { resetUsersTable } from "../db/helpers.js";
+import { seedData } from "../db/seed-data.js";
+
+
+beforeEach(() => {
+  resetUsersTable(seedData) 
+});
+
+
 
 test("Health check route should work!", async () => {
-
-	let r = await request(app).get("/api/health");
-	expect(r.statusCode).toBe(200);
-	expect(r.body.success).toBe(true)
-	expect(r.body.payload).toBe("API is running correctly")
-	expect(r.headers["content-type"]).toBe("application/json; charset=utf-8")
-
-})
-
+  let r = await request(app).get("/api/health");
+  expect(r.statusCode).toBe(200);
+  expect(r.body.success).toBe(true);
+  expect(r.body.payload).toBe("API is running correctly");
+  expect(r.headers["content-type"]).toBe("application/json; charset=utf-8");
+});
 
 test("Users route should be functional", async () => {
-	
-	let result = await request(app).get("/api/users");
-	expect(result.statusCode).toBe(200);
-    expect(result.body.success).toBe(true)
-	expect(Array.isArray(result.body.payload)).toBe(true)
-	expect('id' in result.body.payload[0]).toBe(true)
-	expect('username' in result.body.payload[0]).toBe(true)
-	expect(result.headers["content-type"]).toBe("application/json; charset=utf-8")
-})
- 
- 
+  let result = await request(app).get("/api/users");
+  expect(result.statusCode).toBe(200);
+  expect(result.body.success).toBe(true);
+
+  expect(Array.isArray(result.body.payload)).toBe(true);
+  expect("id" in result.body.payload[0]).toBe(true);
+  expect(typeof result.body.payload[0].id === "number").toBe(true);
+
+  expect("username" in result.body.payload[0]).toBe(true);
+  expect(typeof result.body.payload[0].username === "string").toBe(true);
+
+  expect(result.headers["content-type"]).toBe(
+    "application/json; charset=utf-8"
+  );
+});
+
 //we first checked if payload is actually an Array  expect(Array.isArray(result.body.payload)).toBe(true)
